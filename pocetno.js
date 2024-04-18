1d7ab5b83463b277b55cb141a928fcaf0323cc67
Martina <martinaeling99@gmail.com>
2024-04-18 17:09:10 +0200

    dodan js file - pocetno1

---
+++ pocetno.js
@@ -0,0 +1,29 @@
+//inicijalizacija varijabli
+
+var first_name = "Tony";
+
+var first_name = "John",
+  lastName = "Doe",
+  price = 0,
+  discount = 78,
+  fullPrice = 0,
+  $myObject = {};
+
+console.log(price);
+console.log(lastName);
+
+//kraj mog programa
+
+price = 19.9;
+discount = 0.1;
+
+if (x == 1) {
+  x = 2;
+}
+
+if (a) {
+  var z = 44;
+  return 0;
+}
+
+fullPrice = (price * 100) / discount;
---
+++ prviprimjer.js
@@ -0,0 +1,8 @@
+var x = false;
+
+if (x) { console.log("asfd");
+} 
+console.log("abcd")
+
+var y = null;
+console.log('x= + x')
