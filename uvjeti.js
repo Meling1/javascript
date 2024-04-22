a55b3fd04bbc1fe02805d40ae81535250ae08e3f
office10 <martinaeling99@gmail.com>
2024-04-22 18:07:34 +0200

    napravljeni while i for loop

--- uvjeti.js
+++ uvjeti.js
@@ -10,3 +10,41 @@ if (x === true){
 else if ('moje ime' !== 'moje ime'){ 
     console.log('varijable NISU identične')
 }
+
+//zadatak: 1. Napisite while petlju koja ce ispisivati brojeve od 3 do 20, osim onih djeljivih sa 9.
+
+/*
+var x = 2;
+
+while ( x < 20) {
+    x++;
+
+    if (x < 3) {
+        //console.log('manji od 3');
+        x++; 
+    continue;
+    }
+
+    /* if ( x == 9 || x == 18) {
+        continue;
+    } */
+
+  /*  if (x % 9 === 0) {
+        continue;
+    }
+    
+    console.log('x= ' + x); 
+*/
+
+//2. Koja je vrijednost isprintana u konzoli?
+
+var varOne= 0;
+for (i = 0; i < 4; i++){
+    console.log('iteracija ' + i + ' po varijabli i');
+    for (j = 0; j < 4; j++){
+        console.log('iteracija ' + j + ' po varijabli j');
+        varOne++;
+        console.log('vrijednost=' + varOne);
+    }
+}
+console.log(varOne);
